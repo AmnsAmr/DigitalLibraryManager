c56044549d25d8a0298c52cd95813ef3b3ba6366
AmnsAmr <amraniaamine@gmai.com>
2025-12-06 00:30:18 +0100

    Search function in Library.cs

--- Services/Library.cs
+++ Services/Library.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DigitalLibraryManager.Models;
+using DigitalLibraryManager.Exceptions;
 
 namespace DigitalLibraryManager.Services
 {
     internal class Library
     {
+        private List<Document> _documents = new List<Document>();
+
+        public void AddDocument(Document doc)
+        {
+            _documents.Add(doc);
+        }
+
+        public void DisplayAll()
+        {
+            if (_documents.Count == 0)
+            {
+                Console.WriteLine("The library is empty.");
+                return;
+            }
+            foreach (var doc in _documents)
+            {
+                doc.DisplayDetails();
+            }
+        }
+
+        public void Search(string keyword)
+        {
+            var results = _documents.Where(d =>
+                d.Title.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
+                d.Author.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (results.Count == 0)
+            {
+                //error handeling later
+                Console.WriteLine("No documents found matching that keyword.");
+            }
+            else
+            {
+                foreach (var doc in results) doc.DisplayDetails();
+            }
+        }
     }
 }
