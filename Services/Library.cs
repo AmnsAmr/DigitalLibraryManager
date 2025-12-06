c992fee879d004edc25685771f72c72e70a6a2d5
AmnsAmr <amraniaamine@gmai.com>
2025-12-06 16:23:52 +0100

    Find and remove functions in Library class

--- Services/Library.cs
+++ Services/Library.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using DigitalLibraryManager.Models;
+﻿using DigitalLibraryManager.Models;
 using DigitalLibraryManager.Exceptions;
 
 namespace DigitalLibraryManager.Services
 {
     internal class Library
     {
-        private List<Document> _documents = new List<Document>();
+        private static List<Document> _documents = new List<Document>();
 
         public void AddDocument(Document doc)
         {
@@ -29,21 +25,85 @@ namespace DigitalLibraryManager.Services
             }
         }
 
-        public void Search(string keyword)
+        static private List<Document> Find(string keyword)
         {
-            var results = _documents.Where(d =>
-                d.Title.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
-                d.Author.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
-
-            if (results.Count == 0)
+            try
             {
-                //error handeling later
-                Console.WriteLine("No documents found matching that keyword.");
+                List<Document> results = _documents.Where(d =>
+               d.Title.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
+               d.Author.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+                if (results.Count == 0)
+                {
+                    throw new Exception("No documents found matching that keyword.");
+                }
+                return results;
             }
-            else
+            catch (Exception ex)
             {
-                foreach (var doc in results) doc.DisplayDetails();
+                Console.WriteLine(ex.Message);
+                return new List<Document>();
             }
         }
+
+        public bool Search(string keyword)
+        {
+            try
+            {
+                var results = Library.Find(keyword);
+                foreach (var doc in results) doc.DisplayDetails();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }      
+        }
+
+        public bool Remove(string keyword)
+        {
+            try
+            {
+                foreach (Document D in _documents)
+                {
+                    if (D.Title.Contains(keyword, StringComparison.OrdinalIgnoreCase) || D.Author.Contains(keyword, StringComparison.OrdinalIgnoreCase))
+                    {
+                        _documents.Remove(D);
+                        return true;
+                    }
+                }
+            }
+            catch (DocumentNotFoundException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            return false;
+        }
+
+        public void Add(Document doc) 
+        {
+            _documents.Add(doc);
+        }
+
+
+        public bool Save(string filePath)
+        {
+            foreach (Document doc in _documents)
+            {
+                try
+                {
+
+                }
+                catch (DocumentNotFoundException ex)
+                { 
+                    Console.WriteLine(ex.ToString());
+                }
+                finally 
+                { 
+                }
+            }
+        }
+
     }
 }
